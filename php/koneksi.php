30ac94a31bd8ba5435c6001df30ef52d1643551d
Dani Kurniawan <47886545+dkzhen@users.noreply.github.com>
2022-07-13 08:21:30 +0700

    Update koneksi.php

--- php/koneksi.php
+++ php/koneksi.php
@@ -1,11 +1,6 @@
 <?php
 
-    $host = 'db4free.net:3306'; 
-$dbuser = 'dkzhen19';
-$dbpassword = 'Bandulan113';
-$dbname = 'gamestore_native';
-
-$registrasi = mysqli_connect($host,$dbuser,$dbpassword,$dbname);
+$registrasi = mysqli_connect('db4free.net','dkzhen19','Bandulan113','gamestore-native');
 if($registrasi){
     mysqli_query($registrasi,'SET NAMES uff8');
     echo "Connection - successful </br>";
