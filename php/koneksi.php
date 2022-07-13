b0de04807ec6239670ec4a7590bc8b4d52b74346
Dani Kurniawan <47886545+dkzhen@users.noreply.github.com>
2022-07-13 07:56:15 +0700

    Update koneksi.php

--- php/koneksi.php
+++ php/koneksi.php
@@ -1,9 +1,18 @@
 <?php
 
-    $registrasi = mysqli_connect("db4free.net:3306","dkzhen19","Bandulan113","gamestore_native");
+    $username = dkzhen19; 
+$password = Bandulan113; 
+$host = "db4free.net:3306"; 
+$dbname = gamestore_native; 
 
-    //check koneksi
-    if (mysqli_connect_errno()){
-        echo "Koneksi Database Gagal : " . mysqli_connect_error();
-    }
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'); 
+
+try 
+{ 
+    $registrasi = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options); 
+} 
+catch(PDOException $ex) 
+{ 
+    die("Failed to connect to the database: " . $ex->getMessage()); 
+} 
 ?>
